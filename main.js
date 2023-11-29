edd1522253bcf7800eedf22def710122227185d4
Safekarma <115760171+Safekarma@users.noreply.github.com>
2023-11-28 17:51:58 -0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
+noseX=0;
+noseY=0;
+
+
 function preload()
 {
+    mustache = loadImage('https://i.postimg.cc/3x3QzSGq/m.png');
 }
 
 function setup()
@@ -22,6 +27,7 @@ function modelLoaded()
 function draw()
 {
 image(video, 0, 0, 300, 300);
+image(mustache, noseX, noseY, 40, 40);
 }
 
 function take_snapshot()
@@ -34,7 +40,9 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
-        console.log("nose x = "+ results[0].pose.nose.x);
-        console.log("nose y = "+ results[0].pose.nose.y);
+         noseX = results[0].pose.nose.x - 20;
+         noseY = results[0].pose.nose.y + 5;
+         console.log("nose x = " + noseX);
+         console.log("nose y = " + noseY);
     }
 }
